ce471f625b5c54ba4dbb1c66970da351be314cae
Matheus Brito <matheusbritodasilva@gmail.com>
2025-03-24 14:01:03 -0300

    Update producer.ts

--- src/jobs/producer.ts
+++ src/jobs/producer.ts
@@ -1,13 +1,22 @@
 import Queue from 'bee-queue';
-import { JobPayloads, JobTypes } from './workers';
+import { JobPayloads, JobTypes, QueueType } from './workers';
+import { jobCreatedDuration, jobCreatedWitSuccess } from './observability/producer-metrics';
 
-const queue = new Queue<{ type: JobTypes; payload: JobPayloads[JobTypes] }>('challenge-sre-glob-queue', {
+const queue = new Queue<QueueType>('challenge-sre-glob-queue', {
   redis: process.env.DATABASE_REDIS_URL,
   isWorker: false, 
 });
 
 export const addJob = async <T extends JobTypes>(type: T, payload: JobPayloads[T]) => {
-  await queue.createJob({ type, payload }).save();
+  const end = jobCreatedDuration.startTimer({ queue: 'challenge-sre-glob-queue', job_type: type });
+  try{
+    await queue.createJob({ type, payload }).save();
+    jobCreatedWitSuccess.inc({ queue: 'challenge-sre-glob-queue', job_type: type })
+  }catch{
+    jobCreatedWitSuccess.inc({ queue: 'challenge-sre-glob-queue', job_type: type })
+  }finally{
+    end()
+  }
 };
 
 export { queue };
